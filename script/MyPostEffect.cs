d6d48f1ff4df92430eac69e5eeb00f4b35498c51
SEIYA HONDA <75392537+sh854@users.noreply.github.com>
2025-05-12 08:52:52 +0900

    Update MyPostEffect.cs

--- script/MyPostEffect.cs
+++ script/MyPostEffect.cs
@@ -21,7 +21,6 @@ public class MyPostEffect : ScriptableRendererFeature
     {
         if (scriptablePass != null && scriptablePass.postEffectMaterial != null)
         {
-            // レンダーキューに登録 (ポストエフェクト実行)
             renderer.EnqueuePass(scriptablePass);
         }
     }
@@ -37,23 +36,20 @@ public class MyPostEffect : ScriptableRendererFeature
         public RenderPassEvent renderPassEvent = RenderPassEvent.AfterRenderingTransparents;
     }
 
-    /// <summary>
-    /// Grayscale実行Pass
-    /// </summary>
+   
     class GrayScalePass : ScriptableRenderPass
     {
         private readonly string profilerTag = "GrayScale Pass";
 
-        public Material postEffectMaterial; // グレースケール計算用マテリアル
+        public Material postEffectMaterial; 
 
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
         {
             var cameraColorTarget = renderingData.cameraData.renderer.cameraColorTarget;
 
-            // コマンドバッファ
+         
             var cmd = CommandBufferPool.Get(profilerTag);
 
-            // マテリアル実行
             cmd.Blit(cameraColorTarget, cameraColorTarget, postEffectMaterial);
 
             context.ExecuteCommandBuffer(cmd);
